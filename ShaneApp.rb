a8d40ddfc3f0f39f7f749456ffc5f9e5c21a51f7
Patrick Hurley <phurley@gmail.com>
2008-07-19 16:09:11 -0400

    Add copyright

--- ShaneApp.rb
+++ ShaneApp.rb
@@ -3,7 +3,7 @@
 #  ShaneNotes
 #
 #  Created by Patrick Hurley on 7/19/08.
-#  Copyright (c) 2008 __MyCompanyName__. All rights reserved.
+#  Copyright (c) 2008 HurleyHome LLC. All rights reserved.
 #
 
 require 'osx/cocoa'
@@ -13,7 +13,6 @@ class ShaneApp < OSX::NSObject
   ib_outlet :menu
   
   def applicationDidFinishLaunching(sender)
-      puts "Menu #{@menu.inspect}"
       statusbar = OSX::NSStatusBar.systemStatusBar 
       item = statusbar.statusItemWithLength(OSX::NSVariableStatusItemLength) 
       image = OSX::NSImage.alloc.initWithContentsOfFile("/Users/phurley/projects/ShaneNotes/smile.tiff") 
