821ba8cc91d40b2bd75d5981c560aab48624fed9
Patrick Hurley <phurley@gmail.com>
2008-07-20 13:48:57 -0400

    Notes are saving need hyper links

--- ShaneApp.rb
+++ ShaneApp.rb
@@ -11,13 +11,33 @@ require 'pp'
 
 class ShaneApp < OSX::NSObject
   ib_outlet :menu
-  
+
   def applicationDidFinishLaunching(sender)
       statusbar = OSX::NSStatusBar.systemStatusBar 
       item = statusbar.statusItemWithLength(OSX::NSVariableStatusItemLength) 
       image = OSX::NSImage.alloc.initWithContentsOfFile("/Users/phurley/projects/ShaneNotes/smile.tiff") 
       item.setImage(image) 
-      
+
       item.setMenu(@menu) 
   end
+
+  class << self
+    def notesdir
+      dir = File.join(ENV['HOME'], "Library", "Application Support", "ShaneNotes")
+      Dir.mkdir(dir) rescue nil
+      dir
+    end
+    
+    def files
+      @files ||= Dir.glob(File.join(notesdir, "*.rtf"))
+    end
+    
+    def mkfname(fname)
+      fname = File.basename(fname, ".rtf")
+      File.join(notesdir, fname + ".rtf")
+    end
+    
+  end
+  
 end
+
